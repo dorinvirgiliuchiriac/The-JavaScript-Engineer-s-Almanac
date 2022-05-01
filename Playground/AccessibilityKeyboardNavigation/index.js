4f5c541f18716a100af821601e7c5240a6572b04
Dorin-Virgiliu Chiriac <dorin.virgiliu.chiriac@gmail.com>
2022-05-01 11:15:47 +0300

    added unsubscribe functionality

--- Playground/AccessibilityKeyboardNavigation/index.js
+++ Playground/AccessibilityKeyboardNavigation/index.js
@@ -1,301 +1,310 @@
-(function addAccessibilityKeyboardNavigation() {
-  const ELEMENT_TYPES = {
-    Headers: "Headers",
-    Links: "Links",
-    Landmarks: "Landmarks",
-  };
+var unsubscribeAccessibilityKeyboardNavigation =
+  (function addAccessibilityKeyboardNavigation() {
+    const ELEMENT_TYPES = {
+      Headers: "Headers",
+      Links: "Links",
+      Landmarks: "Landmarks",
+    };
 
-  const ELEMENTS_OF_TYPE = {
-    Header: ["H1", "H2", "H3", "H4", "H5", "H6"],
-    Link: ["A"],
-    Input: ["INPUT", "TEXTAREA", "SELECT"],
-    SkipSomeLandmarkChecksForChildrenOf: [
-      "ARTICLE",
-      "ASIDE",
-      "MAIN",
-      "NAV",
-      "SECTION",
-    ],
-  };
+    const ELEMENTS_OF_TYPE = {
+      Header: ["H1", "H2", "H3", "H4", "H5", "H6"],
+      Link: ["A"],
+      Input: ["INPUT", "TEXTAREA", "SELECT"],
+      SkipSomeLandmarkChecksForChildrenOf: [
+        "ARTICLE",
+        "ASIDE",
+        "MAIN",
+        "NAV",
+        "SECTION",
+      ],
+    };
 
-  const KEY_CODES = {
-    H: 72,
-    L: 76,
-    M: 77,
-    ArrowUp: 38,
-    ArrowDown: 40,
-  };
+    const KEY_CODES = {
+      H: 72,
+      L: 76,
+      M: 77,
+      ArrowUp: 38,
+      ArrowDown: 40,
+    };
 
-  const DIRECTION = {
-    Up: -1,
-    Down: 1,
-  };
+    const DIRECTION = {
+      Up: -1,
+      Down: 1,
+    };
 
-  let state = {
-    elementType: null,
-    direction: DIRECTION.Down,
-    selectedElement: {
-      element: null,
-      initialStyling: null,
-    },
-  };
+    let state = {
+      elementType: null,
+      direction: DIRECTION.Down,
+      selectedElement: {
+        element: null,
+        initialStyling: null,
+      },
+    };
 
-  return subscribeToKeyboardEvents();
+    return subscribeToKeyboardEvents();
 
-  function subscribeToKeyboardEvents() {
-    document.addEventListener("keydown", onKeyDown);
-    return unsubscribeFromKeyboardEvents;
-  }
+    function subscribeToKeyboardEvents() {
+      if (unsubscribeAccessibilityKeyboardNavigation) {
+        unsubscribeAccessibilityKeyboardNavigation();
+      }
 
-  function unsubscribeFromKeyboardEvents() {
-    document.removeEventListener("keydown", onKeyDown);
-  }
-
-  function onKeyDown(event) {
-    if (isActiveElementInput()) {
-      return;
+      document.addEventListener("keydown", onKeyDown);
+      return unsubscribeFromKeyboardEvents;
     }
 
-    switch (event.keyCode) {
-      case KEY_CODES.H:
-        const headers = getListOfAllElementsOfTypes(
-          ELEMENTS_OF_TYPE.Header,
-          document.body
+    function unsubscribeFromKeyboardEvents() {
+      document.removeEventListener("keydown", onKeyDown);
+    }
+
+    function onKeyDown(event) {
+      if (isActiveElementInput()) {
+        return;
+      }
+
+      console.log("yello");
+
+      switch (event.keyCode) {
+        case KEY_CODES.H:
+          const headers = getListOfAllElementsOfTypes(
+            ELEMENTS_OF_TYPE.Header,
+            document.body
+          );
+          moveToNextElement(headers, ELEMENT_TYPES.Headers);
+          break;
+        case KEY_CODES.L:
+          const links = getListOfAllElementsOfTypes(
+            ELEMENTS_OF_TYPE.Link,
+            document.body
+          );
+          moveToNextElement(links, ELEMENT_TYPES.Links);
+          break;
+        case KEY_CODES.M:
+          const landmarks = getListOfAllLandmarks(document.body);
+          moveToNextElement(landmarks, ELEMENT_TYPES.Landmarks);
+          break;
+        case KEY_CODES.ArrowUp:
+          state.direction = DIRECTION.Up;
+          break;
+        case KEY_CODES.ArrowDown:
+          state.direction = DIRECTION.Down;
+          break;
+      }
+    }
+
+    function isActiveElementInput() {
+      return ELEMENTS_OF_TYPE.Input.includes(document.activeElement?.tagName);
+    }
+
+    function getListOfAllElementsOfTypes(types, node) {
+      if (isElementHidden(node)) {
+        return [];
+      }
+
+      const isNodeTypeInTypes = types.includes(node.tagName);
+      const result = isNodeTypeInTypes ? [node] : [];
+
+      if (!node?.children?.length) {
+        return result;
+      }
+
+      const children = Array.from(node.children).reduce((acc, child) => {
+        const children = getListOfAllElementsOfTypes(types, child);
+        return [...acc, ...children];
+      }, []);
+
+      return [...result, ...children];
+    }
+
+    function getListOfAllLandmarks(node, skipSomeLandmarkChecks = false) {
+      if (isElementHidden(node)) {
+        return [];
+      }
+
+      const isLandmark = isNodeLandmark(node, skipSomeLandmarkChecks);
+      const result = isLandmark ? [node] : [];
+
+      if (!node?.children?.length) {
+        return result;
+      }
+
+      const skipSomeLandmarkChecksForChildren =
+        ELEMENTS_OF_TYPE.SkipSomeLandmarkChecksForChildrenOf.includes(
+          node.tagName
         );
-        moveToNextElement(headers, ELEMENT_TYPES.Headers);
-        break;
-      case KEY_CODES.L:
-        const links = getListOfAllElementsOfTypes(
-          ELEMENTS_OF_TYPE.Link,
-          document.body
+
+      const children = Array.from(node.children).reduce((acc, child) => {
+        const children = getListOfAllLandmarks(
+          child,
+          skipSomeLandmarkChecks || skipSomeLandmarkChecksForChildren
         );
-        moveToNextElement(links, ELEMENT_TYPES.Links);
-        break;
-      case KEY_CODES.M:
-        const landmarks = getListOfAllLandmarks(document.body);
-        moveToNextElement(landmarks, ELEMENT_TYPES.Landmarks);
-        break;
-      case KEY_CODES.ArrowUp:
-        state.direction = DIRECTION.Up;
-        break;
-      case KEY_CODES.ArrowDown:
-        state.direction = DIRECTION.Down;
-        break;
-    }
-  }
+        return [...acc, ...children];
+      }, []);
 
-  function isActiveElementInput() {
-    return ELEMENTS_OF_TYPE.Input.includes(document.activeElement?.tagName);
-  }
-
-  function getListOfAllElementsOfTypes(types, node) {
-    if (isElementHidden(node)) {
-      return [];
+      return [...result, ...children];
     }
 
-    const isNodeTypeInTypes = types.includes(node.tagName);
-    const result = isNodeTypeInTypes ? [node] : [];
+    function isNodeLandmark(node, skipSomeChecks) {
+      let result =
+        isFormLandmark(node) ||
+        isNavigationLandmark(node) ||
+        isRegionLandmark(node) ||
+        isSearchLandmark(node);
 
-    if (!node?.children?.length) {
+      if (!result && !skipSomeChecks) {
+        result =
+          isBannerLandmark(node) ||
+          isComplementaryLandmark(node) ||
+          isContentinfoLandmark(node) ||
+          isMainLandmark(node);
+      }
+
       return result;
     }
 
-    const children = Array.from(node.children).reduce((acc, child) => {
-      const children = getListOfAllElementsOfTypes(types, child);
-      return [...acc, ...children];
-    }, []);
-
-    return [...result, ...children];
-  }
-
-  function getListOfAllLandmarks(node, skipSomeLandmarkChecks = false) {
-    if (isElementHidden(node)) {
-      return [];
-    }
-
-    const isLandmark = isNodeLandmark(node, skipSomeLandmarkChecks);
-    const result = isLandmark ? [node] : [];
-
-    if (!node?.children?.length) {
-      return result;
-    }
-
-    const skipSomeLandmarkChecksForChildren =
-      ELEMENTS_OF_TYPE.SkipSomeLandmarkChecksForChildrenOf.includes(
-        node.tagName
+    function isFormLandmark(node) {
+      return (
+        Boolean(node) &&
+        (node.tagName === "FORM" || node.getAttribute("role") === "form") &&
+        hasAriaLabel(node)
       );
-
-    const children = Array.from(node.children).reduce((acc, child) => {
-      const children = getListOfAllLandmarks(
-        child,
-        skipSomeLandmarkChecks || skipSomeLandmarkChecksForChildren
-      );
-      return [...acc, ...children];
-    }, []);
-
-    return [...result, ...children];
-  }
-
-  function isNodeLandmark(node, skipSomeChecks) {
-    let result =
-      isFormLandmark(node) ||
-      isNavigationLandmark(node) ||
-      isRegionLandmark(node) ||
-      isSearchLandmark(node);
-
-    if (!result && !skipSomeChecks) {
-      result =
-        isBannerLandmark(node) ||
-        isComplementaryLandmark(node) ||
-        isContentinfoLandmark(node) ||
-        isMainLandmark(node);
     }
 
-    return result;
-  }
+    function isNavigationLandmark(node) {
+      return (
+        Boolean(node) &&
+        (node.tagName === "NAV" || node.getAttribute("role") === "navigation")
+      );
+    }
 
-  function isFormLandmark(node) {
-    return (
-      Boolean(node) &&
-      (node.tagName === "FORM" || node.getAttribute("role") === "form") &&
-      hasAriaLabel(node)
-    );
-  }
+    function isRegionLandmark(node) {
+      return (
+        Boolean(node) &&
+        (node.tagName === "SECTION" ||
+          node.getAttribute("role") === "region") &&
+        hasAriaLabel(node)
+      );
+    }
 
-  function isNavigationLandmark(node) {
-    return (
-      Boolean(node) &&
-      (node.tagName === "NAV" || node.getAttribute("role") === "navigation")
-    );
-  }
+    function isSearchLandmark(node) {
+      return Boolean(node) && node.getAttribute("role") === "search";
+    }
 
-  function isRegionLandmark(node) {
-    return (
-      Boolean(node) &&
-      (node.tagName === "SECTION" || node.getAttribute("role") === "region") &&
-      hasAriaLabel(node)
-    );
-  }
+    function isBannerLandmark(node) {
+      return (
+        Boolean(node) &&
+        (node.tagName === "HEADER" || node.getAttribute("role") === "banner")
+      );
+    }
 
-  function isSearchLandmark(node) {
-    return Boolean(node) && node.getAttribute("role") === "search";
-  }
+    function isComplementaryLandmark(node) {
+      return (
+        Boolean(node) &&
+        (node.tagName === "ASIDE" ||
+          node.getAttribute("role") === "complementary")
+      );
+    }
 
-  function isBannerLandmark(node) {
-    return (
-      Boolean(node) &&
-      (node.tagName === "HEADER" || node.getAttribute("role") === "banner")
-    );
-  }
+    function isContentinfoLandmark(node) {
+      return (
+        Boolean(node) &&
+        (node.tagName === "FOOTER" ||
+          node.getAttribute("role") === "contentinfo")
+      );
+    }
 
-  function isComplementaryLandmark(node) {
-    return (
-      Boolean(node) &&
-      (node.tagName === "ASIDE" ||
-        node.getAttribute("role") === "complementary")
-    );
-  }
+    function isMainLandmark(node) {
+      return (
+        Boolean(node) &&
+        (node.tagName === "MAIN" || node.getAttribute("role") === "main")
+      );
+    }
 
-  function isContentinfoLandmark(node) {
-    return (
-      Boolean(node) &&
-      (node.tagName === "FOOTER" || node.getAttribute("role") === "contentinfo")
-    );
-  }
+    function hasAriaLabel(node) {
+      return (
+        Boolean(node.getAttribute("aria-labelledby")) ||
+        Boolean(node.getAttribute("aria-label")) ||
+        Boolean(node.getAttribute("title"))
+      );
+    }
 
-  function isMainLandmark(node) {
-    return (
-      Boolean(node) &&
-      (node.tagName === "MAIN" || node.getAttribute("role") === "main")
-    );
-  }
+    function moveToNextElement(elements, elementType) {
+      updateDirection(elementType);
 
-  function hasAriaLabel(node) {
-    return (
-      Boolean(node.getAttribute("aria-labelledby")) ||
-      Boolean(node.getAttribute("aria-label")) ||
-      Boolean(node.getAttribute("title"))
-    );
-  }
+      if (!elements.length) {
+        resetSelectedElement();
+        return;
+      }
 
-  function moveToNextElement(elements, elementType) {
-    updateDirection(elementType);
+      const indexOfActiveElement = elements.indexOf(
+        state.selectedElement.element
+      );
+      let indexOfNextActiveElement = indexOfActiveElement + state.direction;
 
-    if (!elements.length) {
+      if (indexOfNextActiveElement < 0) {
+        indexOfNextActiveElement = 0;
+      }
+      if (indexOfNextActiveElement >= elements.length) {
+        indexOfNextActiveElement = elements.length - 1;
+      }
+
       resetSelectedElement();
-      return;
+      setSelectedElement(elements[indexOfNextActiveElement]);
     }
 
-    const indexOfActiveElement = elements.indexOf(
-      state.selectedElement.element
-    );
-    let indexOfNextActiveElement = indexOfActiveElement + state.direction;
-
-    if (indexOfNextActiveElement < 0) {
-      indexOfNextActiveElement = 0;
-    }
-    if (indexOfNextActiveElement >= elements.length) {
-      indexOfNextActiveElement = elements.length - 1;
+    function updateDirection(elementType) {
+      if (state.elementType !== elementType) {
+        state.elementType = elementType;
+        state.direction = DIRECTION.Down;
+      }
     }
 
-    resetSelectedElement();
-    setSelectedElement(elements[indexOfNextActiveElement]);
-  }
+    function isElementHidden(element) {
+      if (!element) {
+        return true;
+      }
 
-  function updateDirection(elementType) {
-    if (state.elementType !== elementType) {
-      state.elementType = elementType;
-      state.direction = DIRECTION.Down;
-    }
-  }
-
-  function isElementHidden(element) {
-    if (!element) {
-      return true;
+      const styles = getComputedStyle(element);
+      return (
+        styles.display === "none" ||
+        styles.visibility === "hidden" ||
+        element.hidden
+      );
     }
 
-    const styles = getComputedStyle(element);
-    return (
-      styles.display === "none" ||
-      styles.visibility === "hidden" ||
-      element.hidden
-    );
-  }
+    function resetSelectedElement() {
+      if (state.selectedElement.element) {
+        state.selectedElement.element.style =
+          state.selectedElement.initialStyling;
+      }
 
-  function resetSelectedElement() {
-    if (state.selectedElement.element) {
-      state.selectedElement.element.style =
-        state.selectedElement.initialStyling;
+      state.selectedElement = {
+        element: null,
+        initialStyling: null,
+      };
     }
 
-    state.selectedElement = {
-      element: null,
-      initialStyling: null,
-    };
-  }
+    function setSelectedElement(element) {
+      if (!element) {
+        return;
+      }
 
-  function setSelectedElement(element) {
-    if (!element) {
-      return;
-    }
+      state.selectedElement = {
+        element,
+        initialStyling: element.style,
+      };
 
-    state.selectedElement = {
-      element,
-      initialStyling: element.style,
-    };
-
-    element.setAttribute(
-      "style",
-      ` border: 4px solid black !important; 
+      element.setAttribute(
+        "style",
+        ` border: 4px solid black !important; 
         background-color: #fece6d !important; 
         color: black !important;
         box-sizing: border-box !important;`
-    );
-    element.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest",
-    });
-    element.focus();
-  }
-})();
+      );
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "nearest",
+      });
+      element.focus();
+    }
+  })();
